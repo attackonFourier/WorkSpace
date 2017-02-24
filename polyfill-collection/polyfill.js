36427387bf1e5fa2a5aa9fa7d615069dc7182db4
Fourier <543098332@qq.com>
2017-02-24 16:57:35 +0800

    shimming collection
    垫片的集合

---
+++ polyfill-collection/polyfill.js
@@ -0,0 +1,38 @@
+/**
+ * Created by cloud.wong on 2017/2/15.
+ */
+class Polyfill{
+    static install(){
+        Object.setPrototypeOf = Object.setPrototypeOf || function (obj,proto) {
+            obj.__proto__=proto;
+            return obj;
+        };
+
+        // ES6 Object.assign
+        Object.assign = Object.assign || function (target) {
+            if (target === undefined || target === null) {
+                throw new TypeError('Cannot convert undefined or null to object');
+            }
+
+            let output = Object(target);
+            for (let i = 1; i < arguments.length; i++) {
+                let source = arguments[i];
+                if (source !== undefined && source !== null) {
+                    for (let key in source) {
+                        if (source.hasOwnProperty(key)) {
+                            output[key] = source[key];
+                        }
+                    }
+                }
+            }
+            return output;
+        };
+
+        // ES6 Promise (missing support in IE11)
+        if (typeof self.Promise !== 'function') {
+            require('es6-promise').polyfill();
+        }
+    }
+}
+Polyfill.install();
+export default Polyfill;
